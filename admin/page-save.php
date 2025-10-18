1183891dc91e5d5a5527694fc4f2e48e8afd3d72
Michael McDaniel <184707127+Fogles2@users.noreply.github.com>
2025-10-18 10:30:02 -0400

    Add page save logic

---
+++ admin/page-save.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__ . '/../config/config.php';
+require __DIR__ . '/../src/Page.php';
+
+$page = new Page();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $title = $_POST['title'];
+    $slug = $_POST['slug'];
+    $content = $_POST['content'];
+    
+    if ($page->create($title, $content, $slug)) {
+        header('Location: /admin/dashboard.php');
+        exit;
+    } else {
+        echo "Failed to create page.";
+    }
+}
+?>
