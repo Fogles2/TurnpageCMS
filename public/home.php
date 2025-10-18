f68a54a3345b6897b12df76a6a85eab3980cc382
Michael McDaniel <184707127+Fogles2@users.noreply.github.com>
2025-10-18 10:29:47 -0400

    Add home.php as the default page

---
+++ public/home.php
@@ -0,0 +1,6 @@
+<?php include __DIR__ . '/../includes/header.php'; ?>
+
+<h1>Welcome to <?php echo SITE_NAME; ?></h1>
+<p>This is the home page of your new CMS.</p>
+
+<?php include __DIR__ . '/../includes/footer.php'; ?>
